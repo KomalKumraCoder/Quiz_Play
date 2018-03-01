883fe64b21f6002e68f192fc7a87bc7247df1afd
Komal Kumra <36366428+KomalKumraCoder@users.noreply.github.com>
2018-03-01 12:25:23 -0600

    Add files via upload

---
+++ question.js
@@ -0,0 +1,22 @@
+var questions = 
+      [
+        {
+          question: "What is the capital of United Kingdom?",
+          option1: "Manchester", 
+		  option2:"Birmingham", 
+		  option3:"London", 
+		  option4:"Birmingham",
+          answer: 2
+        },
+        
+        {
+          question: "What is the capital of United States?",
+          option1:"California", 
+		  option2:"New York",
+          option3:"Miami", 
+		  option4:"Florida",
+          answer: 1
+        }
+        
+        
+      ];
